c0fe0f0080ab197cbc147648bee3ecda972e389b
Jojito <reynosoyorch@gmail.com>
2020-10-28 07:47:33 -0600

    Se resuelve problema de tamaño de fragments (correccion en tamaño de contenedor y viewPager)

--- app/src/main/java/com/example/agrosmart/Drawer/PolicyFragment.java
+++ app/src/main/java/com/example/agrosmart/Drawer/PolicyFragment.java
@@ -1,9 +1,11 @@
 package com.example.agrosmart.Drawer;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,6 +21,9 @@ public class PolicyFragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.fragment_policy,container,false);
+        TextView textView = (TextView)view.findViewById(R.id.text_policy);
+        textView.setText(R.string.text_policy);
+        textView.setMovementMethod(new ScrollingMovementMethod());
         return view;
     }
 
@@ -26,5 +31,6 @@ public class PolicyFragment extends Fragment
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
     {
         super.onViewCreated(view, savedInstanceState);
+
     }
 }
--- app/src/main/java/com/example/agrosmart/LoginRequest.java
+++ app/src/main/java/com/example/agrosmart/LoginRequest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 public class LoginRequest extends StringRequest
 {
-    private static final String loginRequest_URL = "http://192.168.100.17/AgroSmartPhp/login_register/Login.php";
+    private static final String loginRequest_URL = "http://192.168.0.104/AgroSmartPhp/login_register/Login.php";
     private Map<String, String> params;
 
     public LoginRequest(String Email, String Password, Response.Listener<String> listener)
